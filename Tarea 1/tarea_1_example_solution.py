17bb5a01bf1de2714cf7765b315dc7d99f524ac6
Mauricio Garcia <mauricioga117@gmail.com>
2024-02-15 22:32:28 -0600

    Se agregaron más comentarios al archivo tarea_1_example_solution.py y se corrigieron los errores marcados por flake8.

--- Tarea 1/tarea_1_example_solution.py
+++ Tarea 1/tarea_1_example_solution.py
@@ -1,10 +1,15 @@
-# La funcion invert case recibe una cadena y tiene el siguiente comportamiento
+# La funcion invert case recibe un solo parametro "cadena" que es un string
+# y retorna lo siguiente
 
 # Caso de éxito => 0
 
 # Error en caso de que la entrada no sea un string => -16
 # Error en caso de un caracter fuera del abecedario => -32
 # Error en caso de un string vacio => -48
+
+# Parametros
+# Entrada: Un string llamado cadena
+# Salida: La cadena invertida (exito) o None (error) y un codigo
 
 def invert_case(cadena):
     invertida = ""
